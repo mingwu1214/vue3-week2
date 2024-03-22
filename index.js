7f5cd9da3405b36583d24183419268b881e65e5c
Ming Wu <mingwu1214@gmail.com>
2024-03-22 12:28:49 +0800

    init import

---
+++ index.js
@@ -0,0 +1,45 @@
+import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
+const baseUrl = "https://vue3-course-api.hexschool.io/v2/";
+const apiPath = 'mingwu';
+
+const app = {
+  data(){
+    return {
+      tempProduct: {},
+      products: {}
+    }
+  },
+  methods: {
+    checkAdmin() {
+      const url = `${baseUrl}api/user/check`;
+      axios.post(url)
+        .then(() => {
+          this.getProducts();
+        })
+        .catch((err) => {
+          alert(err.response.data.message);
+          window.location = 'login.html';
+        })
+    },
+    getProducts(){
+      const url = `${baseUrl}api/${apiPath}/admin/products/all`;
+      axios.get(url)
+        .then((res) => {
+          this.products = res.data.products;
+        })
+        .catch((err) => {
+          alert(err.response.data.message);
+        })
+    },
+    openProduct(item) {
+      this.tempProduct = item;
+    }
+  },
+  mounted(){
+    //get token
+    const token = document.cookie.replace(/(?:(?:^|.*;\s*)mingToken\s*=\s*([^;]*).*$)|^.*$/, '$1');
+    axios.defaults.headers.common.Authorization = token;
+    this.checkAdmin()
+  }
+}
+createApp(app).mount('#app');
---
+++ login.js
@@ -0,0 +1,29 @@
+import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js';
+const baseUrl = "https://vue3-course-api.hexschool.io/v2/";
+
+const app = {
+  data(){
+    return {
+      user: {
+        username: '',
+        password: ''
+      },
+    }
+  },
+  methods: {
+    login(){
+      const signinApi = `${baseUrl}admin/signin`;
+      axios.post(signinApi, this.user)
+        .then((res) => {
+          const { expired, token } = res.data;
+          //將 token 存進 cookie
+          document.cookie = `mingToken=${token}; expires=${new Date(expired)};`;
+          window.location = 'index.html'
+        })
+        .catch((err) => {
+          alert(err.response.data.message)
+        })
+    }
+  }
+};
+createApp(app).mount("#app");
